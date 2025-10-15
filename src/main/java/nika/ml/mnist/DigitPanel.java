71e0cdabb4d553b973bccd643f134066f430ba21
vektor-y <unika.yurk@gmail.com>
2025-10-15 13:08:34 +0300

    style: update images description

--- src/main/java/nika/ml/mnist/DigitPanel.java
+++ src/main/java/nika/ml/mnist/DigitPanel.java
@@ -1,7 +1,11 @@
 package nika.ml.mnist;
 
+import nika.ml.network.Result;
+
 import javax.swing.*;
 import java.awt.*;
+
+import static java.lang.Math.round;
 
 class DigitPanel extends JPanel {
 
@@ -17,16 +21,17 @@ class DigitPanel extends JPanel {
         super.paintComponent(g);
         int size = 28;   // MNIST image size
         int scale = 10;   // enlarge MNIST pixels
-        for (int i = 0; i < result.getImage().length; i++) {
+        for (int i = 0; i < result.getInput().length; i++) {
             int x = i % size;
             int y = i / size;
-            int val = 255 - result.getImage()[i]; // invert so higher means darker
+            int val = 255 - (int) round(result.getInput()[i] * 255); // invert so higher means darker
             g.setColor(new Color(val, val, val));
             g.fillRect(x * scale, y * scale, scale, scale);
         }
         g.setColor(Color.BLACK);
         int textY = size * scale + 15;
-        g.drawString(String.valueOf("Prediction: " + result.getLabel()), 5, textY);
-        g.drawString(String.format("Confidence: %.2f %%", result.getConfidence()), 5, textY + 15);
+        g.drawString("Digit: " + result.getLabel()
+                + "   |   Prediction: " + result.getPrediction()
+                + String.format("   |   Confidence: %.2f %%", result.getConfidence()), 5, textY);
     }
 }
